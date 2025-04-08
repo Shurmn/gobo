083465963c0daa239814a0cc903d5d3265a66107
JR Sh <jrs>
2025-04-07 23:12:32 -0400

    Add some menu stuff and goof around on the state machine

--- scripts/core/State.gd
+++ scripts/core/State.gd
@@ -1,2 +1,25 @@
 class_name State
 extends Node
+
+# the state wants to change
+signal state_aspires_to(desired_state: String)
+
+# the stator will give us unhandled inputs from this chute
+func handle_input(event: InputEvent) -> void:
+	pass
+
+# we don't want to use _process, that's for the stator to decide
+func update(_delta: float) -> void:
+	pass
+
+# same idea, let the stator control when this happens	
+func physics_update(_delta: float) -> void:
+	pass
+
+# for init things, lookin at you animation
+func on_enter() -> void:
+	pass
+
+# dont let the door hit ya
+func on_exit() -> void:
+	pass
--- scripts/core/Stator.gd
+++ scripts/core/Stator.gd
@@ -1,5 +1,42 @@
 class_name Stator
 extends Node
 
+# what can we be
 var states = {}
-@export var current_state: State
+
+# what have we been
+var history = []
+
+# how we begin
+var initial_state: State
+
+# what are we now
+@onready var current_state: State = get_initial_state()
+
+# init not set but that's null good dawg we'll just go for the first child State
+func get_initial_state() -> State:
+	return initial_state if initial_state != null else get_child(0)
+
+# connect to the state_aspires_to signal of all child states, add them to states
+func _ready() -> void:
+	for child in get_children():
+		if child is State:
+			child.state_aspires_to.connect(_on_state_shift)
+			states[child.name] = child
+	await owner.ready
+	current_state.on_enter()
+
+func _unhandled_input(event: InputEvent) -> void:
+	current_state.handle_input(event)
+
+func _process(delta: float) -> void:
+	current_state.update(delta)
+
+func _physics_process(delta: float) -> void:
+	current_state.physics_update(delta)
+
+func _on_state_shift(new_state) -> void:
+	current_state.on_exit()
+	current_state = new_state
+	current_state.on_enter()
+	
