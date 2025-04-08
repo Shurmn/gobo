ad61f27509d3b916dfaafca1debba14fbb6c0094
JR Sh <jrs>
2025-04-08 01:36:40 -0400

    Twiddle a bit more with state. Some stubs for ideas. lite re-dir

---
+++ scripts/core/Data.gd
@@ -0,0 +1,5 @@
+class_name Data
+extends Resource
+
+# not so fast lois, the boat's a boat, but the mystery box could be anything.
+var data = {}
--- scripts/core/Stator.gd
+++ scripts/core/Stator.gd
@@ -13,11 +13,11 @@ var initial_state: State
 # what are we now
 @onready var current_state: State = get_initial_state()
 
-# init not set but that's null good dawg we'll just go for the first child State
+# if init isn't, that's null good dawg we'll just get the first child
 func get_initial_state() -> State:
 	return initial_state if initial_state != null else get_child(0)
 
-# connect to the state_aspires_to signal of all child states, add them to states
+# connect our state_aspires_to signals, use names as ref key in states
 func _ready() -> void:
 	for child in get_children():
 		if child is State:
@@ -25,7 +25,19 @@ func _ready() -> void:
 			states[child.name] = child
 	await owner.ready
 	current_state.on_enter()
+	
+# status in motu est. we wait for the signal to change
+func _on_state_shift(desired_state: String) -> void:
+	shift_state_to(states.get(desired_state, initial_state))
 
+# exit, remember, switch, enter
+func shift_state_to(new_state: State) -> void:
+	current_state.on_exit()
+	history.append(current_state.name)
+	current_state = new_state
+	current_state.on_enter()
+
+# at last we call these three. now we are only ever processing a single state
 func _unhandled_input(event: InputEvent) -> void:
 	current_state.handle_input(event)
 
@@ -34,9 +46,3 @@ func _process(delta: float) -> void:
 
 func _physics_process(delta: float) -> void:
 	current_state.physics_update(delta)
-
-func _on_state_shift(new_state) -> void:
-	current_state.on_exit()
-	current_state = new_state
-	current_state.on_enter()
-	
